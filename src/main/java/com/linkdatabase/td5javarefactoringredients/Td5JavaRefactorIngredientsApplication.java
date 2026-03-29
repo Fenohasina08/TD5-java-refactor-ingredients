8445afdc70461acc43e57a837d1b7169946eadb3
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-29 13:23:13 +0300

    feat: initia commit

---
+++ src/main/java/com/linkdatabase/td5javarefactoringredients/Td5JavaRefactorIngredientsApplication.java
@@ -0,0 +1,13 @@
+package com.linkdatabase.td5javarefactoringredients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td5JavaRefactorIngredientsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Td5JavaRefactorIngredientsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linkdatabase/td5javarefactoringredients/Td5JavaRefactorIngredientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkdatabase.td5javarefactoringredients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td5JavaRefactorIngredientsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
