4d084950e7578cc637208fc0922f354124c854ac
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-30 08:39:03 +0300

    feat: ajust DishController

--- src/main/java/com/linkdatabase/td5javarefactoringredients/controller/DishController.java
+++ src/main/java/com/linkdatabase/td5javarefactoringredients/controller/DishController.java
@@ -2,35 +2,33 @@ package com.linkdatabase.td5javarefactoringredients.controller;
 
 import com.linkdatabase.td5javarefactoringredients.entity.Dish;
 import com.linkdatabase.td5javarefactoringredients.entity.Ingredient;
-import com.linkdatabase.td5javarefactoringredients.exception.NotFoundException;
-import com.linkdatabase.td5javarefactoringredients.repository.DishRepository;
-import org.apache.coyote.BadRequestException;
+import com.linkdatabase.td5javarefactoringredients.service.DishService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@AllArgsConstructor
 public class DishController {
 
-    private final DishRepository dishRepository = new DishRepository();
+    private final DishService dishService;
 
     @GetMapping("/dishes")
     public List<Dish> getAllDishes() {
-        return dishRepository.findAllWithIngredients();
+        return dishService.findAllWithIngredients();
+    }
+
+    @GetMapping("/dishes/{id}")
+    public Dish getDishById(@PathVariable int id) {
+        return dishService.findById(id);
     }
 
     @PutMapping("/dishes/{id}/ingredients")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void updateDishIngredients(@PathVariable int id,
-                                      @RequestBody(required = false) List<Ingredient> ingredients) throws BadRequestException {
-        if (ingredients == null) {
-            throw new BadRequestException("Request body is required");
-        }
-        Dish dish = dishRepository.findById(id);
-        if (dish == null) {
-            throw new NotFoundException("Dish.id=" + id + " is not found");
-        }
-        dishRepository.updateIngredients(id, ingredients);
+                                      @RequestBody(required = false) List<Ingredient> ingredients) {
+        dishService.updateDishIngredients(id, ingredients);
     }
 }
